f9db6256b9f7ef400400e99181de542875b7b0f4
evarice kuete <evaricekuete2@gmail.com>
2026-07-09 10:55:30 +0200

    edit

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
  * Ce fichier contient les identifiants de base de données et les constantes d'environnement.
  * Il est recommandé de garder ce fichier hors de la vue du public.
  */
-/* paramettres de configuration du serveur de données en ligne*/
+/* paramettres de configuration du serveur de données en ligne   */
 
    define('DB_HOST', 'localhost');
    define('DB_NAME', 'u290233073_col_futura_db');
@@ -28,12 +28,13 @@
     define('APP_URL', 'http://localhost:8000');
 
     define('APP_ENV', 'development');
-    define('DEBUG_MODE', true); */ //affichage des erreures PHP au developpeur
+    define('DEBUG_MODE', true); */    //affichage des erreures PHP au developpeur
 
 /* Force error display for debugging   
   
    ini_set('display_errors', 1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
-  */ 
   
+  
+*/ 
