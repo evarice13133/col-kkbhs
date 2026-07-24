26e65efd2e33fa98bfb5861ed8532ef0bdb8e17a
evarice kuete <evaricekuete2@gmail.com>
2026-07-24 11:52:16 +0200

    mise à jour du QR code

--- config/config.php
+++ config/config.php
@@ -28,13 +28,13 @@
     define('APP_URL', 'http://localhost:8000');
 
     define('APP_ENV', 'development');
-    define('DEBUG_MODE', true);   */  //affichage des erreures PHP au developpeur
+    define('DEBUG_MODE', true);    */ //affichage des erreures PHP au developpeur
 
-/* Force error display for debugging   
+/* Force error display for debugging  
   
    ini_set('display_errors', 1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
-  */
+  */ 
   
  
