a00517c06545644a153f0bf0351e1f37acde3001
evarice kuete <evaricekuete2@gmail.com>
2026-08-25 03:43:37 +0200

    mise à jour 2 sur config

--- config/config.php
+++ config/config.php
@@ -5,20 +5,20 @@
  * Ce fichier contient les identifiants de base de données et les constantes d'environnement.
  * Il est recommandé de garder ce fichier hors de la vue du public.
  */
-/* paramettres de configuration du serveur de données en ligne   */
+/* paramettres de configuration du serveur de données en ligne   
 
-   define('DB_HOST', 'localhost');
-   define('DB_NAME', 'u290233073_col_col_kkbhs');
-   define('DB_USER', 'u290233073_root_col_kkbhs');
+  define('DB_HOST', 'localhost');
+   define('DB_NAME', 'u290233073_col_futura_db');
+   define('DB_USER', 'u290233073_root_futura');
    define('DB_PASS', 'Tezempa13133!!');
    define('DB_CHARSET', 'utf8mb4');
-   define('APP_URL', 'https://col-kkbhs.camertech.com');
+   define('APP_URL', 'https://futura.camertech.com');
 
    define('APP_ENV', 'production');
-   define('DEBUG_MODE', false); //evite l'affichage des erreures PHP au utilisateurs en production
+   define('DEBUG_MODE', false);*/ //evite l'affichage des erreures PHP au utilisateurs en production
 
 
-/* en developpement pour la configuration locale 
+/* en developpement pour la configuration locale */ 
    
    define('DB_HOST', 'localhost');
     define('DB_NAME', 'u290233073_col_col_kkbhs_db1');
@@ -28,13 +28,13 @@
     define('APP_URL', 'http://localhost:8000');
 
     define('APP_ENV', 'development');
-    define('DEBUG_MODE', true);  */   //affichage des erreures PHP au developpeur
+    define('DEBUG_MODE', true);    //affichage des erreures PHP au developpeur
 
-/* Force error display for debugging   
+/* Force error display for debugging   */ 
   
    ini_set('display_errors', 1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
- */ 
-  
  
+  git@github.com:evarice13133/futura.camertech.git    -     https://webhooks.hostinger.com/deploy/5c268448f78945b471bbef333ea10955
+  git@github.com:evarice13133/col-kkbhs.git           -     https://webhooks.hostinger.com/deploy/cd6d548d7e495bd4ec18fa3c3f7b2e44
